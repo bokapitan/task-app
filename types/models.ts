99137c0cc78bd64a9415a03606e20910c678550f
B. Kapitanker <bokapitan@gmail.com>
2025-12-03 19:03:35 -0500

    preparing UI elements for subtask

--- types/models.ts
+++ types/models.ts
@@ -1,10 +1,23 @@
 import { Database } from "@/lib/database.types";
 
+// 1. Add the Subtask definition manually
+export type Subtask = {
+  subtask_id: string;
+  task_id: string;
+  title: string;
+  is_completed: boolean;
+  created_at: string;
+};
+
 // Database Models
-export type Task = Database["public"]["Tables"]["tasks"]["Row"];
+// We use a trick here: we take the database row and JOIN it with an optional array of subtasks
+export type Task = Database["public"]["Tables"]["tasks"]["Row"] & {
+  subtasks?: Subtask[]; 
+};
+
 export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
 
-// Extended User type that includes additional fields not in the database
+// Extended User type
 export type User = Profile & {
   email: string;
   tasks_created: number;
